9906c03e8f945ad18c6670faf6bbf9332cff7f8b
Itzblitz98 <gfxscene@gmail.com>
2015-05-01 23:16:08 +0100

    better pause play and stop

--- integrate.js
+++ integrate.js
@@ -1,119 +1,130 @@
 "use strict";
 
-(function(Nuvola)
-{
+(function(Nuvola) {
 
-    // Create media player component
-    var player = Nuvola.$object(Nuvola.MediaPlayer);
+  // Create media player component
+  var player = Nuvola.$object(Nuvola.MediaPlayer);
 
-    // Handy aliases
-    var PlaybackState = Nuvola.PlaybackState;
-    var PlayerAction = Nuvola.PlayerAction;
+  // Handy aliases
+  var PlaybackState = Nuvola.PlaybackState;
+  var PlayerAction = Nuvola.PlayerAction;
 
-    // Create new WebApp prototype
-    var WebApp = Nuvola.$WebApp();
+  // Create new WebApp prototype
+  var WebApp = Nuvola.$WebApp();
 
-    // Initialization routines
-    WebApp._onInitWebWorker = function(emitter)
-    {
-        Nuvola.WebApp._onInitWebWorker.call(this, emitter);
+  // Initialization routines
+  WebApp._onInitWebWorker = function(emitter) {
+    Nuvola.WebApp._onInitWebWorker.call(this, emitter);
 
-        var state = document.readyState;
-        if (state === "interactive" || state === "complete")
-            this._onPageReady();
-        else
-            document.addEventListener("DOMContentLoaded",
-                    this._onPageReady.bind(this));
+    var state = document.readyState;
+    if (state === "interactive" || state === "complete")
+      this._onPageReady();
+    else
+      document.addEventListener("DOMContentLoaded",
+        this._onPageReady.bind(this));
+  }
+
+  // Page is ready for magic
+  WebApp._onPageReady = function() {
+    // Connect handler for signal ActionActivated
+    Nuvola.actions.connect("ActionActivated", this);
+
+    // Set default action states
+    player.setCanPlay(true);
+    player.setCanPause(true);
+    //player.setCanStop(false);
+    player.setCanGoPrev(false);
+    player.setCanGoNext(false);
+
+    // Configure API hooks
+    this.startApi();
+  }
+
+  // Loads the KEXP flowplayer API
+  WebApp.startApi = function() {
+
+    if (window.API.getMedia()) {
+      this.update();
+    } else {
+      setTimeout(this.startApi.bind(this), 100);
+    }
+  }
+
+
+  // Extract data from the web page
+  WebApp.update = function() {
+    if (window.API.getMedia()) {
+      // Scrape track info
+      var track = window.API.getMedia().title;
+      var artist = window.API.getMedia().author;
+      var album = "";
+      var art = "http:" + window.API.getMedia().image;
+      var track = {
+        title: track,
+        artist: artist,
+        album: album,
+        artLocation: art
+      };
+      player.setTrack(track);
+    } else {
+      var track = {
+        title: "",
+        artist: "",
+        album: "",
+        artLocation: ""
+      };
     }
 
-    // Page is ready for magic
-    WebApp._onPageReady = function()
-    {
-        // Connect handler for signal ActionActivated
-        Nuvola.actions.connect("ActionActivated", this);
-
-        // Set default action states
-        player.setCanPlay(false);
-        player.setCanPause(true);
-				//player.setCanStop(false);
-        player.setCanGoPrev(false);
-        player.setCanGoNext(false);
-
-        // Configure API hooks
-        this.startApi();
+    if (document.getElementById("playback-controls").classList.contains("snoozed")) {
+      var state = PlaybackState.PAUSED;
+    } else {
+      var state = PlaybackState.PLAYING;
     }
 
-    // Loads the KEXP flowplayer API
-    WebApp.startApi = function()
-    {
+    player.setPlaybackState(state);
 
-			if(window.API.getMedia()){
-				this.update();
-			} else {
-				setTimeout(this.startApi.bind(this), 100);
-			}
-    }
+    // Schedule the next update
+    setTimeout(this.update.bind(this), 500);
+  }
 
+  // Handler of playback actions
+  WebApp._onActionActivated = function(emitter, name, param) {
 
-    // Extract data from the web page
-    WebApp.update = function()
-    {
-      if(window.API.getMedia()){
-        // Scrape track info
-        var track = window.API.getMedia().title;
-        var artist = window.API.getMedia().author;
-        var album = "";
-        var art = "http:" + window.API.getMedia().image;
-        var track = {
-            title: track,
-            artist: artist,
-            album: album,
-            artLocation: art
-        };
-        player.setTrack(track);
-      } else {
-        var track = {
-            title: "",
-            artist: "",
-            album: "",
-            artLocation: ""
-        };
-      }
-				if(window.API.getVolume() > 0){
-					var state = PlaybackState.PLAYING;
-				} else {
-					var state = PlaybackState.PAUSED;
-				}
-
-
-        player.setPlaybackState(state);
-
-        // Schedule the next update
-        setTimeout(this.update.bind(this), 500);
-    }
-
-    // Handler of playback actions
-    WebApp._onActionActivated = function(emitter, name, param)
-    {
-				console.log(name);
-        switch(name)
-        {
-					case PlayerAction.STOP:
-						window.API.setVolume("0");
-					break;
-					case PlayerAction.TOGGLE_PLAY:
-					window.API.setVolume("100");
-					break;
-          case PlayerAction.PLAY:
-						window.API.setVolume("100");
-						console.log("play?");
-          break;
-          case PlayerAction.PAUSE:
-						window.API.setVolume("0");
-						console.log("Pause?");
-          break;
+    switch (name) {
+      case PlayerAction.TOGGLE_PLAY:
+        if (document.getElementById("playback-controls").classList.contains("snoozed")) {
+          document.getElementById("playback-controls").getElementsByClassName("refresh")[0].click();
+        } else {
+          document.getElementById("playback-controls").getElementsByClassName("snooze")[0].click();
         }
-    }
 
-    WebApp.start();
+        console.log("toggle play");
+        //button refresh
+        break;
+      case PlayerAction.PLAY:
+        document.getElementById("playback-controls").getElementsByClassName("refresh")[0].click();
+        console.log("play");
+        break;
+      case PlayerAction.PAUSE:
+        document.getElementById("playback-controls").getElementsByClassName("snooze")[0].click();
+        console.log("pause");
+        break;
+      case PlayerAction.STOP:
+        document.getElementById("playback-controls").getElementsByClassName("snooze")[0].click();
+        console.log("stop");
+        break;
+      case PlayerAction.PREV_SONG:
+        //not supported
+        break;
+      case PlayerAction.NEXT_SONG:
+        //not supported
+        break;
+      default:
+        throw {
+          "message": "Not supported."
+        };
+    }
+  }
+
+  WebApp.start();
 })(this);
